f02e6c01dca807a3b1dccc4b190d5b6b773e42a0
KimSJ <kim@spencejones.com>
2014-05-17 01:20:08 +0100

    add hdlc receiver byte spotting; create first testbench

--- hdlcreceiver.vhd
+++ hdlcreceiver.vhd
@@ -10,14 +10,14 @@ use IEEE.NUMERIC_STD.all;
 use std.textio.all;
 use ieee.std_logic_textio.all;
 
-entity HdlcTransmitter is
+entity HdlcReceiver is
 	generic (
 		rxReqChainSize  : integer := 2 -- defines length of metastability chain; must be at least two.
 	);
 	port (
 		-- microprocesser interface
 		Dout :		out	Std_Logic_Vector (7 downto 0); -- rx register
-		rxLast : 	in 	Std_Logic;
+		rxLast : 	out Std_Logic;
 		rxRD : 		in	Std_Logic; -- read strobe
 		rxReq : 	out	Std_Logic; -- high if data available
 
@@ -27,7 +27,7 @@ entity HdlcTransmitter is
 		sysClk :	in	Std_Logic; -- 16MHz
 
 		-- line interface
-		rxD :		buffer	Std_Logic;
+		rxD :		in	Std_Logic;
 		rxEn :		buffer	Std_Logic
 	);
 -- translate_off
@@ -37,9 +37,92 @@ begin
 	report "rxReqChainSize should be at least 2!"
 	severity ERROR;
 -- translate_on
-end HdlcTransmitter;
+end HdlcReceiver;
 
-architecture behavioural of HdlcTransmitter is
+architecture behavioural of HdlcReceiver is
+
+	signal shiftReg :	Std_Logic_Vector (7 downto 0);
+	signal bitClk :		Std_Logic;
+	signal bitCount :	Std_Logic_Vector (2 downto 0);
+	signal onesCount :	Std_Logic_Vector (2 downto 0);
+	signal flagRx, abortRx : Std_Logic;
+
+	signal clkDiv : Std_Logic_Vector(5 downto 0);
+	signal data_change : boolean;
+	signal data_was : Std_Logic := '1';
+	signal skipBit : boolean;
+	signal skippedBit : boolean;
+	signal flagSpotted : boolean;
+
+
+	function To_Std_Logic(L: BOOLEAN) return std_ulogic is 
+    begin 
+		if L then 
+			return('1'); 
+		else 
+			return('0'); 
+		end if; 
+    end function To_Std_Logic;
+
+    function IncrementSLV(V: Std_Logic_Vector) return Std_Logic_Vector is
+    begin
+    	return Std_Logic_Vector(unsigned(V) + unsigned'("1"));
+	end function IncrementSLV;
+
 
 begin
+	pBitClk: process (sysClk)
+		variable edge_detect : Std_Logic_Vector (2 downto 0);
+	begin
+		bitClk <= clkDiv(5);
+		data_change <= data_was /= edge_detect(2) and data_was /= edge_detect(1) and data_was /= edge_detect(0);
+		if rising_edge(sysClk) then
+			edge_detect := rxD & edge_detect(2 downto 1);
+			if data_change then
+				clkDiv <= "000011";
+				data_was <= not data_was;
+			else
+				clkDiv <= IncrementSLV(clkDiv);
+			end if;
+		end if;
+	end process pBitClk;
+
+	pShiftReg: process (bitClk, rxRST)
+	begin
+		skipBit <= shiftReg(7 downto 2) = "111110" and rxD = '0' and not skippedBit;
+		flagSpotted <= onesCount = "110" and rxD = '0'; -- "01111110" received
+		if rxRST='1' then
+			shiftReg <= (others => '1');
+			onesCount <= (others => '0');
+			flagRx <= '0';
+			abortRx <= '0';
+			bitCount <= (others => '0');
+		else		
+			if rising_edge(bitClk) and not skipBit then
+				shiftReg <= rxD & shiftReg(7 downto 1);
+				if flagSpotted then
+					bitCount <= "000";
+				else
+					bitCount <= incrementSLV(bitCount);
+				end if;
+				if bitCount <= "000" then
+					Dout <= shiftReg;
+				end if;
+			end if;
+			if rising_edge(bitClk) then
+				skippedBit <= skipBit;
+				if onesCount /= "111" then -- count to 7 then stop
+					if rxD = '0' then
+						onesCount <= "000";
+					else
+						onesCount <= Std_Logic_Vector(unsigned(onesCount) + unsigned'("1"));
+					end if;
+				end if;
+				flagRx <= To_Std_Logic(flagSpotted); -- latch flag spotted
+				abortRx <= To_Std_Logic((onesCount = "110" and rxD = '1') or onesCount = "111"); -- seven or more '1's received
+			end if;
+		end if;
+
+	end process pShiftReg;
+
 end behavioural;
---
+++ hdlcreceiver_tb.vhd
@@ -0,0 +1,157 @@
+-- arduinointerface.vhd
+--
+-- takes 8-bit parallel data and sends frame
+-- Frame ends when data value is written with "rxLast" set.
+
+-- connect data to low 4 bits of port
+-- connect strb to b4 of port (configured as output)
+-- connect RnW to b5 of port (configured as output)
+
+-- to read this peripheral:
+--		(assuming strb is left high between accesses)
+--		set port low bits to input
+--      set RmW, strb to 1, 0 (10 = command "read low-nibble")
+--		read the value
+--		set strb 1 (11 = command "read high-nibble)
+--		read the value
+-- for multi-byte reads, repeat last four steps
+
+-- to write this peripheral:
+--		(assuming strb is left high between accesses)
+--      set RnW to 0 (strb no change, so no write yet; output buffers now disabled)
+--		set port low bits to output
+--		write the lo-nibble value, with b5, b4 = 00
+--		write the hi-nibble value, with b5, b4 = 01
+-- for multi-byte writes, repeat last two steps
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.All;
+use IEEE.NUMERIC_STD.all;
+-- debug libraries
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
+entity hdlcreceiver_tb is
+
+end entity hdlcreceiver_tb;
+
+architecture behavioural of hdlcreceiver_tb is
+	signal q	:  	Std_Logic_Vector (7 downto 0); -- q is the values read
+	signal rxLast : Std_Logic; -- the databus 
+	signal rxRD  : 	Std_Logic := '1';
+	signal rxReq : 	Std_Logic;
+	signal	rst:  	Std_Logic :='1';
+	
+	signal clock: 	Std_Logic := '1';
+
+	signal rxD, rxEn: Std_Logic;
+
+	signal cycle : 	integer :=0;
+
+	component hdlcreceiver is
+	generic (
+		rxReqChainSize  : integer := 2
+	);
+	port (
+		-- microprocesser interface
+		Dout :		out	Std_Logic_Vector (7 downto 0); -- rx register
+		rxLast : 	out 	Std_Logic;
+		rxRD : 		in	Std_Logic; -- read strobe
+		rxReq : 	out	Std_Logic; -- high if data available
+
+		rxRST :		in	Std_Logic;
+
+		-- bit clock
+		sysClk :	in	Std_Logic; -- 16MHz
+
+		-- line interface
+		rxD :		in	Std_Logic;
+		rxEn :		buffer	Std_Logic
+	);
+	end component hdlcreceiver;
+
+	type tDataItem is record
+		rxD: 	Std_Logic;
+		rxRST: 	Std_Logic;
+		rxRD: 	Std_Logic;
+	end record tDataItem;
+
+	type tDataList is array (0 to 25) of tDataItem;
+
+	constant dataList : tDataList := (
+		-- test abort
+			('0','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+		-- test abort clear
+			('0','1','0'),
+		-- test flag detect
+			('0','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('0','0','0'),
+		-- test inserted bit removal
+			('0','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('1','0','0'),
+			('0','0','0')
+		);
+
+begin
+
+	iface : hdlcreceiver
+	port map (
+		Dout => q,
+		rxLast => rxLast,
+		rxRD => rxRd,
+		rxReq => rxReq,
+
+		rxRST => rst,
+
+		-- bit clock
+		sysClk => clock,
+
+		-- line interface
+		rxD => rxD,
+		rxEn => rxEn
+	);
+
+--	rst <= '1' after 0 ns, '0' after 100 ns; 
+	process
+	-- drive the txClock
+	begin
+	 		clock <= '0';
+	 		wait for 62 ns;
+	 		clock <= '1';
+	 		wait for 63 ns;
+	end process;
+
+	process (clock)
+	begin
+		if rising_edge(clock) then
+	 		cycle <= (cycle + 1) mod (64 * dataList'length);
+	 		if cycle = 0 then
+--	 			i <= (i(0) & i(7 downto 1)) xor "0" & i(0) & "00" & i(0) & "0" & i(0) & "0";
+	 		end if;
+	 		if (cycle mod 64) = 0 then
+				rxD <= dataList(cycle/64).rxD;
+				rst <= dataList(cycle/64).rxRST;
+			end if;
+		end if;
+	end process;
+end behavioural;
